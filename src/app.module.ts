5b0b76f6538c3fb0dd580258e6a183d9060cf2da
hauleguitare <alexander.dutyone@gmail.com>
2022-10-28 12:20:46 +0700

    ADD(IsUserExist): Create validation user signup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { IsUserAlreadyExistConstraint } from './common/decorators/Is-user-already-exist.decorator';
 import { DatabaseModule } from './database/database.module';
+import { UserModule } from './users/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, IsUserAlreadyExistConstraint],
 })
 export class AppModule {}
