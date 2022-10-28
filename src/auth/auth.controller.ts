1b68a8903af0af5b2e6d9e5f5c9316c17a96b583
hauleguitare <alexander.dutyone@gmail.com>
2022-10-28 12:50:29 +0700

    FEAT(AuthController, AuthModule): Refactor AuthController and import LocalAuth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,20 +2,25 @@ import {
   Body,
   Controller,
   Post,
+  Req,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { Request } from 'express';
 import { CreateUserDto } from 'src/users/models/create-user.dto';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 import { SignInDto } from './models/sign-in.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  async Login(@Body() signinDto: SignInDto) {
-    return this.authService.Login(signinDto);
+  async Login(@Req() request: Request) {
+    return request.user;
   }
 
   @Post('signup')
