e17d1b3f3171a53d1e05d58878526998c2e99719
hauleguitare <alexander.dutyone@gmail.com>
2022-10-28 12:28:40 +0700

    ADD(TagEntity): Create tag Entity

---
+++ src/entities/tag.entity.ts
@@ -0,0 +1,20 @@
+import {
+  Column,
+  Entity,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
+import { PostEntity } from './post.entity';
+
+@Entity('tag')
+export class TagEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string;
+
+  @ManyToMany(() => PostEntity, (post) => post.tags)
+  posts: PostEntity[];
+}
