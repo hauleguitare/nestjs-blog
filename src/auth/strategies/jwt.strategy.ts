3b09dec874b46813ae6dce785972eac41599b311
hauleguitare <alexander.dutyone@gmail.com>
2022-10-28 14:21:39 +0700

    ADD(JwtStrategy, JwtAuthGuard): Create Jwt Strategy adn Jwt Auth Guard

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Strategy, StrategyOptions, ExtractJwt } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SERCET_KEY'),
+    });
+  }
+
+  async validate(payload: any) {
+    return payload;
+  }
+}
