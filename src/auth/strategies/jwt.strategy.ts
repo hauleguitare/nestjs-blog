16d3ed9cbb26584c25fdcf608047460ef5d36cb8
hauleguitare <alexander.dutyone@gmail.com>
2022-10-30 19:50:17 +0700

    CHORE(AuthModule): Create Enum for Authenticate, adding for Strategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -2,10 +2,14 @@ import { Strategy, StrategyOptions, ExtractJwt } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Authentication } from 'src/common/enum/auth.enum';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
+export class JwtStrategy extends PassportStrategy(
+  Strategy,
+  Authentication.JWT,
+) {
+  constructor(protected configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
