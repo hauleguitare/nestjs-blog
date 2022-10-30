f33b08e33503ecef40d06c84ccc8e9e9065e5127
hauleguitare <alexander.dutyone@gmail.com>
2022-10-30 20:01:29 +0700

    FEAT(AuthController): Adding route for refresh token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import {
   Body,
   Controller,
   Get,
+  HttpCode,
   Post,
   Req,
   UseGuards,
@@ -14,12 +15,13 @@ import { UserDto } from 'src/users/models/user.dto';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
-import { SignInDto } from './models/sign-in.dto';
+import { RefreshJWTAuthGuard } from './guards/refresh-jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @HttpCode(200)
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async Login(@Req() request: Request) {
@@ -36,5 +38,15 @@ export class AuthController {
   @Get('protected')
   async getProtected(@Req() request: Request) {
     return request.user;
+  }
+
+  @UseGuards(RefreshJWTAuthGuard)
+  @Get('refresh')
+  async refreshToken(@Req() request: Request) {
+    const refreshToken = request
+      .get('authorization')
+      ?.replace('Bearer', '')
+      .trim() as string;
+    return this.authService.refreshToken(refreshToken);
   }
 }
